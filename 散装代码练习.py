1beeda2e97ddce2b4d3bdedb01ca707cbb71a21f
boxiangLi <53144287+boxiangLi@users.noreply.github.com>
2019-09-25 15:54:30 +0800

    Update 散装代码练习.py

--- 散装代码练习.py
+++ 散装代码练习.py
@@ -130,3 +130,15 @@ def wait():
     time.sleep(10)
 a=wait()
 print(a)
+#复习39
+#进度条的实例
+import time
+scale = 10
+print("----执行开始----")
+for i in range(scale+1):
+    a='*' * i
+    b='.' * (scale-i)
+    c=(i/scale)*100
+print("{:^3.0f}%[{}->{}]".format(c,a,b))
+time.sleep(10)
+print("-----执行结束-----")
