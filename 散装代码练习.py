602158eb42f9aed90506a7141262e1b387a17ed4
boxiangLi <53144287+boxiangLi@users.noreply.github.com>
2019-10-07 23:42:29 +0800

    Update 散装代码练习.py

--- 散装代码练习.py
+++ 散装代码练习.py
@@ -258,3 +258,17 @@ random.choice([1,2,3,4,5,6])
 import random
 s=[1,2,3,4,5,6,7,8,9];random.shuffle(s);print(s)
 random.random()
+#练习62
+from random import random
+from time import perf_counter
+DARTS=1000*1000
+hits=0.0
+start=perf_counter()
+for i in range (1,DARTS+1):
+    x,y=random(),random()
+    dist=pow(x**2,0.5)
+    if dist<=1.0:
+        hits=hits+1
+    pi=4*(hits/DARTS)
+print("圆周值是：{}".format(pi))
+print("运行时间是:5f}".format(perf_counter()-start))
