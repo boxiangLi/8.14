47861abe8a17ba02080a1fb826d205774c669137
boxiangLi <53144287+boxiangLi@users.noreply.github.com>
2019-12-19 10:27:02 +0800

    Update 复习题练习.cpp

--- 复习题练习.cpp
+++ 复习题练习.cpp
@@ -1,1 +1,62 @@
-
+//复习题1
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"学习语言都是从模仿开始的"<<endl;
+		
+}
+//复习2
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"学习c++的捷径是看例子"<<endl;
+		
+}
+//复习3
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"##############################\n"
+	<<"我是游戏19的李博祥，学号:93\n"
+	<<"##############################\n"<<endl;
+		
+}
+//复习4
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=789,b=621;
+	int c;
+	c=a-b;
+	cout<<c<<endl;		
+}
+//复习5
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"请输入一个正整数:"<<endl; 
+	cin>>a; cout<<"+"<<endl; 
+	cin>>b; cout<<"="<<endl; 
+	c=a+b; 
+	cout<<c<<endl;
+}
+//复习6
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a,b,c,d;
+	cout<<"请输入长方形的长和宽:"<<"\n"<<"边长："<<endl; 
+	cin>>a;
+	cout<<"宽：" <<endl;
+	cin>>b;
+	c=a*b;
+	d=(a+b)*2;
+	cout<<"长方形的面积为："<<c<<"\t"<<"长方形的周长为："<<d<<endl;
+}
