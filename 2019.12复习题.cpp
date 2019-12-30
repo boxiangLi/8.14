2a4c2cf388a8d862e8ed07e4b949b2fc3ef3038a
boxiangLi <53144287+boxiangLi@users.noreply.github.com>
2019-12-30 14:50:05 +0800

    Update 2019.12复习题.cpp

--- 2019.12复习题.cpp
+++ 2019.12复习题.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,i=1;
+	cin>>a>>b;
+	if(a>b)
+	{
+		c=a,a=b,b=c;
+	}
+	d=b*i;
+	while(d%a != 0)
+	{
+		i++;
+		d=b*i;
+	}
+	cout<<d<<endl;
+	
+ } 
