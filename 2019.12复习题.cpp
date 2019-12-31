5d616c07d652407a324bf257e94437a37a5bd402
boxiangLi <53144287+boxiangLi@users.noreply.github.com>
2019-12-31 10:35:23 +0800

    Update 2019.12复习题.cpp

--- 2019.12复习题.cpp
+++ 2019.12复习题.cpp
@@ -1,3 +1,4 @@
+//A1
 #include <iostream>
 using namespace std;
 int main()
@@ -17,3 +18,27 @@ int main()
 	cout<<d<<endl;
 	
  } 
+//A2
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[28],sum=0;
+	int j;
+	int ave;
+	cout<<"输入二十八个整数"<<endl;
+	for(int i=0; i<28; i++)
+	{
+		cin>>a[i];
+		sum=sum+a[i];
+	 } 
+	 ave=sum/28;
+	 for(j=0;j<28;j++)
+	 {
+	 	if(a[j]>ave)
+	 	{
+		cout<<a[j]<<endl;
+	    }
+	 }
+	 return 0;
+}
